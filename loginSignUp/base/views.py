6a374cfbdcfcba55dcf6e51c6b0457de71e57160
rz_ash <rzashfak@gmail.com>
2025-01-22 12:15:12 +0530

    setuped a login, signup pages and viewes py

--- loginSignUp/base/views.py
+++ loginSignUp/base/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+def authView(request):
+    return render(request, "registration/signup.html", {"form": form})
